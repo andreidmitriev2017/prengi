3464a21060adbf8db8bbceee4687a5103adc010b
Andrei Dmitriev <andreid.dmitrieff2017@mail.ru>
2020-08-30 19:07:48 +0500

    Добавлены анимационные эффекты

--- src/js/script.js
+++ src/js/script.js
@@ -11,9 +11,37 @@ $(document).ready(function(){
             }
         }]
     });
+
     $('ul.products__menu').on('click', 'li:not(.products__item_active)', function() {
         $(this)
           .addClass('products__item_active').siblings().removeClass('products__item_active')
           .closest('div.container').find('div.slick-slide').removeClass('slick-current slick-active').eq($(this).index()).addClass('slick-current slick-active');
     }); 
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1300){
+            $('.page-up').fadeIn();
+        }
+        else{
+            $('.page-up').fadeOut();
+        };
+    });
+
+    $("a[href=#up]").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.header__wrapper'),
+        menuItem = document.querySelectorAll('.header__wrapper'),
+        hamburger = document.querySelector('.header__hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('header__hamburger_active');
+            menu.classList.toggle('header__wrapper_active');
+        });
+    });
+
+    new WOW().init();
 });
